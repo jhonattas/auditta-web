3ebc012d8834ad9a865161c50d517deabcb47ef4
Jhonattas Henrique <jhonattas@soucriador.com>
2020-11-16 20:41:03 -0300

    remove os arquivos antigos, pois nao sao mais necessarios e adiciona um redirecionamento

--- application/views/components/background-movie.php
+++
@@ -1,6 +0,0 @@
-<?php
-  defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-<video autoplay loop class="bg_video">
-    <source src="<?php echo base_url('assets/video/home_background.mp4" type="video/mp4');?>">
-</video>
